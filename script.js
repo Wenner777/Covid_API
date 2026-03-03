b0a67070971c7564d0fe8a444a624448b47be8a9
Wenner <wennerguilherme25@gmail.com>
2026-03-03 20:27:23 -0300

    Adicionando funcao para verficar estado do site

---
+++ script.js
@@ -0,0 +1,37 @@
+const onOff = document.querySelector("#onOff");
+
+//* Função para verificar se o site está online
+verificaçãoAPI();
+function verificaçãoAPI(){
+    fetch("https://covid19-brazil-api.now.sh/api/status/v1")
+    .then(res => res.json())
+    .then(dados => {
+
+        let receber = null;
+        
+        if(dados.status === "ok"){
+           receber = 1; 
+        } else {
+            receber = 0;
+        }
+
+        mostrarStatus(receber);
+        
+    })
+    .catch(() => {
+        console.log("Erro!");
+        
+    })
+}
+
+//* Exibe a condição atual do site online/offline
+function mostrarStatus(dados){
+    if(dados === 1){
+        console.log("online");
+        
+    } else if(dados === 0){
+        console.log("offline");
+        
+    }
+    
+}
