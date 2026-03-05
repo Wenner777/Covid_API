17dd137e122a34656f0ec9bbbc4e40fb5b7f2458
Wenner <wennerguilherme25@gmail.com>
2026-03-04 21:41:03 -0300

    Sistema de Busca por pais implementado

--- script.js
+++ script.js
@@ -2,52 +2,69 @@ const onOff = document.querySelector("#onOff");
 
 //* Função para verificar se o site está online
 verificaçãoAPI();
-function verificaçãoAPI(){
+function verificaçãoAPI() {
     fetch("https://covid19-brazil-api.now.sh/api/status/v1")
-    .then(res => res.json())
-    .then(dados => {
+        .then(res => res.json())
+        .then(dados => {
 
-        let receber = null;
-        
-        if(dados.status === "ok"){
-           receber = 1; 
-        } else {
-            receber = 0;
-        }
+            let receber = null;
 
-        mostrarStatus(receber);
-        
-    })
-    .catch(() => {
-        console.log("Erro!");
-        
-    })
+            if (dados.status === "ok") {
+                receber = 1;
+            } else {
+                receber = 0;
+            }
+
+            mostrarStatus(receber);
+
+        })
+        .catch(() => {
+            console.log("Erro!");
+
+        })
 }
 
 //* Exibe a condição atual do site online/offline
-function mostrarStatus(dados){
-    if(dados === 1){
+function mostrarStatus(dados) {
+    if (dados === 1) {
         onOff.textContent = `ONLINE`;
 
-    } else if(dados === 0){
+    } else if (dados === 0) {
         onOff.textContent = `OFFLINE`;
-        
+
     }
-    
+
 }
 
+/**
+ * Nessa parte o codigo vai pegar o select é busca a informação na API 
+*/
+
+// Variaveis do Select
 const infoPais = document.querySelector("#infoPais");
-casosPais();
+const selectPaises = document.querySelector("#selectPais");
+const btnSelecionarPaises = document.querySelector("#buttonSelect");
 
-function casosPais(){
-    fetch("https://covid19-brazil-api.now.sh/api/report/v1/brazil")
-    .then(res => res.json())
-    .then(dados => {
-        const casos = dados.cases;
-        exibirPais(casos);
-    })
+//* Função para chamar
+btnSelecionarPaises.addEventListener("click", () => {
+    const valorDoselect = selectPaises.value;   
+
+    console.log(valorDoselect);
+    
+    casosPais(valorDoselect);
+
+});
+
+function casosPais(pais) {
+    fetch(`https://covid19-brazil-api.vercel.app/api/report/v1/${pais}`)
+        .then(res => res.json())
+        .then(dados => {
+            const dadosPais = dados.data.cases;
+
+            exibirPais(dadosPais);
+        })
 }
 
-function exibirPais(casos){
+function exibirPais(casos) {
     infoPais.innerHTML = `${casos}`;
 }
